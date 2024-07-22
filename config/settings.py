d1d8627cd7b2b4a15cde258fd6c23efdb94830ec
webcam <pr.pypblrka@gmail.com>
2024-07-22 09:14:47 +0400

    вынесены в переменные окружения

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -66,12 +66,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "drf_project",
-        "USER": "postgres",
-        "PASSWORD": "1523618095",
-        "HOST": "127.0.0.1",
-        "PORT": 5432,
+        "ENGINE": 'django.db.backends.postgresql_psycopg2',
+        "NAME": os.getenv("NAME"),
+        "USER": os.getenv("USER"),
+        "PASSWORD": os.getenv("PASSWORD"),
+        "HOST": os.getenv("HOST"),
+        "PORT": os.getenv("PORT"),
     }
 }
 
